53fa6c7a2eef7f678d971a2a1b180c0e1a6486e2
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-03-17 01:20:07 +0300

    Add BaseResponse and PagedResult classes
    Introduce `BaseResponse<T>` for standardized API responses
    and `PagedResult<T>` for handling paginated data.
    Remove commented-out constructor code from `BaseResponse<T>`.

---
+++ src/ASP.NET-API-Template.Core/Models/Response/BaseResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace ASP.NET_API_Template.Core.Models.Response;
+public class BaseResponse<T>
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public T? Data { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public List<string>? Errors { get; set; }
+
+
+    //public BaseResponse(T? data, string message, List<string>? errors = null, bool success = true)
+    //{
+    //    Data = data;
+    //    Message = message;
+    //    Errors = errors;
+    //    Success = success;
+    //}
+}
---
+++ src/ASP.NET-API-Template.Core/Models/Response/PagedResult.cs
@@ -0,0 +1,16 @@
+﻿namespace ASP.NET_API_Template.Core.Models.Response;
+
+public class PagedResult<T>
+{
+    public required List<T> Items { get; set; }
+
+    public int TotalItems { get; set; }
+
+    public int TotalPages { get; set; }
+
+    public int CurrentPage { get; set; }
+
+    public int PageSize { get; set; }
+    public bool HasNextPage => PageSize * CurrentPage < TotalItems;
+    public bool HasPreveiosPage => CurrentPage > 1;
+}
