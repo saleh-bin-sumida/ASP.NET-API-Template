7dc3750ad218e38985c5ebcdc4309464da14ea9c
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-03-17 01:18:57 +0300

    Add DisplayEnumNameExtension for nullable Enum display names
    This commit introduces a new static class `DisplayEnumeNameExtension` in the `ASP.NET_API_Template.Core.Extentions` namespace. It includes an extension method `GetDisplayName` for nullable `Enum` types, which retrieves the display name using the `DisplayAttribute`. The method handles null values and exceptions gracefully, returning an empty string or the enum's string representation as needed.

---
+++ src/ASP.NET-API-Template.Core/Extentions/DisplayEnumeNameExtension.cs
@@ -0,0 +1,24 @@
+﻿namespace ASP.NET_API_Template.Core.Extentions;
+
+public static class DisplayEnumeNameExtension
+{
+    public static string GetDisplayName(this Enum? enumValue)
+    {
+        try
+        {
+            if (enumValue is null)
+            {
+                return "";
+            }
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<DisplayAttribute>()
+                            ?.GetName() ?? enumValue.ToString();
+        }
+        catch
+        {
+            return enumValue.ToString();
+        }
+    }
+}
