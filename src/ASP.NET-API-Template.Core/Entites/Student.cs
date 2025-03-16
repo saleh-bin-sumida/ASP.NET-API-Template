ebef34786a2b7b1f86152b114820a757f0daf9ac
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-03-17 01:18:29 +0300

    Add Student class with entity and soft delete features
    Introduces a new `Student` class in the `ASP.NET_API_Template.Core.Entities` namespace. This class implements the `IEntity` and `ISoftDeletable` interfaces and includes properties for `Id`, `FullName`, `Email`, `PhoneNumber`, `IsDeleted`, `CreatedDate`, `ModifiedDate`, and `DeletedDate`, with `CreatedDate` set to the current date and time.

---
+++ src/ASP.NET-API-Template.Core/Entites/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace ASP.NET_API_Template.Core.Entites;
+
+
+public class Student : IEntity, ISoftDeletable
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public DateTime? ModifiedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+}
