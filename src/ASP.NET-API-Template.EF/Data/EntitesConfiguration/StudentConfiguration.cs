88a815bb09efef08024eaa619928612df77281f8
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-03-17 01:21:44 +0300

    Add StudentConfiguration for Entity Framework setup
    Introduces the `StudentConfiguration` class to configure the `Student` entity.
    Sets table name, property length constraints, applies a global filter for deleted students,
    and seeds initial student data in the database.

---
+++ src/ASP.NET-API-Template.EF/Data/EntitesConfiguration/StudentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace ASP.NET_API_Template.EF.Data.Configuration;
+
+internal class StudentConfiguration : IEntityTypeConfiguration<Student>
+{
+    public void Configure(EntityTypeBuilder<Student> builder)
+    {
+        builder.ToTable(TablesNames.Students);
+
+
+        builder.Property(e => e.FullName)
+            .HasMaxLength(100);
+
+        builder.Property(e => e.PhoneNumber)
+            .HasMaxLength(20);
+
+        builder.Property(e => e.Email)
+            .HasMaxLength(255);
+
+        // Global filter to exclude deleted clients
+        builder.HasQueryFilter(e => !e.IsDeleted);
+
+        builder.HasData(SeedData.SeedStudents());
+    }
+}
