a1c723df5e56481a595b831027e6cfadca839980
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-03-17 01:23:46 +0300

    Add SystemApiRouts for student API endpoints
    Introduced a new static class `SystemApiRouts` in the
    `ASP.NET_API_Template.API.Helper` namespace. This class
    includes a nested static class `Students` that defines
    constant string routes for various student API operations,
    such as retrieving, adding, updating, and deleting students.
    The base route is set to "api/v1/Students".

---
+++ src/ASP.NET-API-Template.API/Helper/SystemApiRouts.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP.NET_API_Template.API.Helper;
+public static class SystemApiRouts
+{
+    public static class Students
+    {
+        public const string Base = "api/v1/Students";
+        public const string GetStudentById = Base + "/{Id}";
+        public const string GetAllCleint = Base;
+        public const string AddStudent = Base;
+        public const string UpdateStudent = Base;
+        public const string DeleteStudent = Base + "/{Id}";
+    }
+}
