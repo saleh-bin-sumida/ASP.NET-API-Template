b43cf1aaa067b5cc11899f024e35302b1ac5d4c8
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-03-17 01:22:37 +0300

    Add UnitOfWork class for repository management
    Introduces a new `UnitOfWork` class in the `ASP.NET_API_Template.EF.Repositories` namespace. This class implements the `IUnitOfWork` interface, manages an `AppDbContext` instance, provides access to a `StudentRepository`, and includes methods for disposing of the context and saving changes asynchronously.

---
+++ src/ASP.NET-API-Template.EF/Repositories/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿namespace ASP.NET_API_Template.EF.Repositories;
+
+public class UnitOfWork(AppDbContext _appDbContext) : IUnitOfWork
+{
+    public IStudentRepository Students { get; private set; } = new StudentRepository(_appDbContext);
+
+
+
+    public void Dispose()
+    {
+        _appDbContext.Dispose();
+    }
+
+    public async Task<int> SaveAsync()
+    {
+        return await _appDbContext.SaveChangesAsync();
+    }
+}
