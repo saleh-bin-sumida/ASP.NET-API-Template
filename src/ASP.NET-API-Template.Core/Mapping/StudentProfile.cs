9cbe9a7d89a32a909bea7d9dca16b9fb72e85b9b
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-03-17 01:19:51 +0300

    Add StudentProfile class with mapping methods
    Introduces a new static class `StudentProfile` in the
    `ASP.NET_API_Template.Core.Mapping` namespace. This class
    includes methods for converting between `Student` and
    `GetStudentDto`, as well as creating a `Student` from
    `AddStudentDto` and updating a `Student` with
    `UpdateStudentDto` values.

---
+++ src/ASP.NET-API-Template.Core/Mapping/StudentProfile.cs
@@ -0,0 +1,33 @@
+﻿namespace ASP.NET_API_Template.Core.Mapping;
+
+public static class StudentProfile
+{
+    public static Expression<Func<Student, GetStudentDto>> ToGetStudentDto()
+    {
+        return client => new GetStudentDto
+        {
+            Id = client.Id,
+            FullName = client.FullName,
+            Email = client.Email,
+            PhoneNumber = client.PhoneNumber,
+        };
+    }
+
+    public static Student ToStudent(this AddStudentDto addStudentDto)
+    {
+        return new Student
+        {
+            FullName = addStudentDto.FullName,
+            Email = addStudentDto.Email,
+            PhoneNumber = addStudentDto.PhoneNumber,
+        };
+    }
+
+    public static void UpdateStudent(this Student client, UpdateStudentDto updateStudentDto)
+    {
+        client.FullName = updateStudentDto.FullName;
+        client.Email = updateStudentDto.Email;
+        client.PhoneNumber = updateStudentDto.PhoneNumber;
+        client.ModifiedDate = DateTime.Now;
+    }
+}
