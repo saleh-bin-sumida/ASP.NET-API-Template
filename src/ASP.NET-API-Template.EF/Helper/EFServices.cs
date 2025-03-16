c855f482d6b183045d0718b889868fb76aac70a2
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-03-17 01:22:58 +0300

    Add EFServices class and global using directives
    Introduces a new static class `EFServices` in the `ASP.NET_API_Template.EF.Helper` namespace, providing a method to add Entity Framework services to the service collection. The method retrieves the "LocalConnection" string from configuration, registers `IUnitOfWork` and `AppDbContext`, and configures `AppDbContext` for SQL Server with sensitive data logging enabled.
    
    Also adds several global using directives in `GlobalUsings.cs` for improved namespace accessibility across the project.

---
+++ src/ASP.NET-API-Template.EF/Helper/EFServices.cs
@@ -0,0 +1,27 @@
+﻿namespace ASP.NET_API_Template.EF.Helper;
+
+public static class EFServices
+{
+    /// <summary>
+    /// Adds the Entity Framework services to the service collection.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <param name="configuration">The configuration.</param>
+    public static void AddEFServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("LocalConnection");
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("Connection string 'RemoteConnection' is not configured.");
+        }
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+            options.EnableSensitiveDataLogging();
+        });
+    }
+}
