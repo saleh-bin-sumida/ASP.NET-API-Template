f9b2c872fbac53e46269c424b2c0be333226fba0
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-03-17 01:19:39 +0300

    Add interfaces for entity management and student repository
    This commit introduces three new interfaces: `IEntity`, `ISoftDeletable`, and `IStudentRepository` in the `ASP.NET_API_Template.Core.Interfaces` namespace.
    
    - `IEntity` defines properties for entity management, including `Id`, `CreatedDate`, and an optional `ModifiedDate`.
    - `ISoftDeletable` provides properties for soft deletion, such as `IsDeleted` and an optional `DeletedDate`.
    - `IStudentRepository` extends a base repository for `Student` and includes asynchronous methods for retrieving, adding, updating, and deleting student records.

---
+++ src/ASP.NET-API-Template.Core/Interfaces/IStudentRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP.NET_API_Template.Core.Interfaces;
+
+public interface IStudentRepository : IBaseRepository<Student>
+{
+    public Task<GetStudentDto> GetStudentById(int id);
+    public Task<PagedResult<GetStudentDto>> GetAllStudents(
+        int pageNumber,
+        int pageSize,
+        string? searchTerm = null);
+    public Task<BaseResponse<string>> AddStudentAsync(AddStudentDto clientDto);
+    public Task<BaseResponse<string>> UpdateStudentAsync(UpdateStudentDto clientDto);
+    public Task<BaseResponse<string>> DeleteStudentAsync(int id);
+}
