b662dbdcb3e3f640349044d110b960779347ffb2
Saleh Mohammed <84684414+saleh-bin-sumida@users.noreply.github.com>
2025-05-04 15:48:14 +0300

    Update BaseResponse.cs

--- src/ASP.NET-API-Template.Core/Models/Response/BaseResponse.cs
+++ src/ASP.NET-API-Template.Core/Models/Response/BaseResponse.cs
@@ -4,18 +4,29 @@ public class BaseResponse<T>
     public bool Success { get; set; }
     public string Message { get; set; }
 
+
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public T? Data { get; set; }
+
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public List<string>? Errors { get; set; }
 
+    public BaseResponse(bool success, string message, T? data = default, List<string>? errors = null)
+    {
+        Success = success;
+        Message = message;
+        Data = data;
+        Errors = errors;
+    }
 
-    //public BaseResponse(T? data, string message, List<string>? errors = null, bool success = true)
-    //{
-    //    Data = data;
-    //    Message = message;
-    //    Errors = errors;
-    //    Success = success;
-    //}
+    public static BaseResponse<T> SuccessResponse(string message = "Operation successful", T? data = default)
+    {
+        return new BaseResponse<T>(true, message, data);
+    }
+
+    public static BaseResponse<T> ErrorResponse(string message, List<string>? errors = null, T? data = default)
+    {
+        return new BaseResponse<T>(false, message, data, errors);
+    }
 }
